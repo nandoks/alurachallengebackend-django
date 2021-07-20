2f01e0439bd015f20ff960a09f11fc00d2393451
nandoksman <fernandooksman@gmail.com>
2021-07-20 15:25:16 -0300

    modify project urls

--- aluraflix/urls.py
+++ aluraflix/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from video.views import VideoViewSet
 
 router = routers.DefaultRouter()
-router.register('videos', VideoViewSet, basename='videos')
+router.register('videos', VideoViewSet, basename='Videos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
